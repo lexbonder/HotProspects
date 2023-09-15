fcdbb7576467a2c2e90de5eae7027205fb215213
Alex Bonder <lexbonder@gmail.com>
2023-09-15 08:20:45 -0400

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Alex Bonder on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

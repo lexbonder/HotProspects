ff78794f77985e3960f6d69e1d437d60ad08d8ed
Alex Bonder <lexbonder@gmail.com>
2023-09-15 08:34:58 -0400

    Hacking with Swift - 100 Days of SwiftUI - Environment Objects

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,14 +7,35 @@
 
 import SwiftUI
 
+@MainActor class User: ObservableObject {
+    @Published var name = "Bug Hunter"
+}
+
+struct EditView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        TextField("Name", text: $user.name)
+    }
+}
+
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        Text(user.name)
+    }
+}
+
 struct ContentView: View {
+    @StateObject var user = User()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            EditView()
+            DisplayView()
         }
+        .environmentObject(user)
         .padding()
     }
 }

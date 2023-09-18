7261d4772bcbe7698e2b68048bfaa2234377060b
Alex Bonder <lexbonder@gmail.com>
2023-09-18 08:10:50 -0400

    Hacking with Swift - 100 Days of SwiftUI - objectWillChange

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,13 +7,34 @@
 
 import SwiftUI
 
+@MainActor class DelayedUpdater: ObservableObject {
+    // @Published var value = 0 // broadcasts that the change is happening.
+    
+    // Using this structure means we get to include side effects. We can add behavior before and after.
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+    
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
+}
+
 struct ContentView: View {
+    @StateObject private var updater = DelayedUpdater()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Value is \(updater.value)")
         }
         .padding()
     }

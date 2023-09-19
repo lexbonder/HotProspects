995b21f3539ae3c467ddd39930475a00b4c20193
Alex Bonder <lexbonder@gmail.com>
2023-09-19 06:41:23 -0400

    Hacking with Swift - 100 Days of Swift UI - Context Menus

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var backgroundColor = Color.red
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .padding()
+                .background(backgroundColor)
+            
+            Text("Change Color")
+                .padding()
+                .contextMenu {
+                    Button {
+                        backgroundColor = .red
+                    } label: {
+                        Label("Red", systemImage: "checkmark.circle.fill")
+//                            .foregroundColor(.red) // foreground color is ignored - can add .destructive role to button, but like, why...
+                    }
+                    
+                    Button("Blue") {
+                        backgroundColor = .blue
+                    }
+                    
+                    Button("Green") {
+                        backgroundColor = .green
+                    }
+                }
         }
-        .padding()
     }
 }
 
